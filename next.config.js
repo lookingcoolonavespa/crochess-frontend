5c127d1cfbf133784d14f8b9c9602fafe919dae2
alex <alexlhliang@gmail.com>
2022-06-26 11:41:22 -0700

    added image optimizers

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const withPlugins = require('next-compose-plugins');
-const withOptimizedImages = require('next-optimized-images');
 const optimizedImages = require('next-optimized-images');
 
-const nextConfig = withOptimizedImages({
-  reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-});
+const nextConfig = withPlugins([
+  [
+    optimizedImages,
+    {
+      reactStrictMode: true,
+      basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+      assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    },
+  ],
+]);
 
 module.exports = nextConfig;
