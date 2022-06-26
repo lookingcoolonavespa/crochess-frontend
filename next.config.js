5ed0c5e34690b9a8963ef9e207bcedc21d0331f2
alex <alexlhliang@gmail.com>
2022-06-26 11:36:57 -0700

    installed next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPlugins = require('next-compose-plugins');
+const withOptimizedImages = require('next-optimized-images');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfig = withOptimizedImages({
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-};
+});
 
 module.exports = nextConfig;
