e67ae6aa17b316309aebded564899f513ce82692
alex <alexlhliang@gmail.com>
2022-06-26 17:54:10 -0700

    next.config now using withPlugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
+import { withPlugins } from 'next-compose-plugins';
 import withOptimizedImages from 'next-optimized-images';
 
-const nextConfig = withOptimizedImages({
+const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-});
+};
 
-export default nextConfig;
+export default withPlugins([withOptimizedImages], nextConfig);
